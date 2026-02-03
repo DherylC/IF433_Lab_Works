954f111ba86429b4bae18053ab63c5e9fae1dd91
DherylC <dheryl.c@gmail.com>
2026-02-03 12:39:14 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000113470_dherylchristoferyahya/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000113470_dherylchristoferyahya.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Name: " + name + " Score: " + score)
+}
