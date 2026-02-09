34e73ed19521e5be75386861897a44bd2dce2e9a
DherylC <dheryl.c@gmail.com>
2026-02-09 13:15:30 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000113470_dherylchristoferyahya/week02/Student.kt
@@ -0,0 +1,11 @@
+package oop_00000113470_dherylchristoferyahya.week02
+
+import javax.swing.plaf.nimbus.NimbusStyle
+
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String
+) {
+
+}
