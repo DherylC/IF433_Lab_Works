8efdbb1814e0643216ac4fbe92a713be45c6059f
DherylC <dheryl.c@gmail.com>
2026-02-09 14:20:33 +0700

    week02: (task 1) implement library input system

--- src/oop_00000113470_dherylchristoferyahya/week02/Loan.kt
+++ src/oop_00000113470_dherylchristoferyahya/week02/Loan.kt
@@ -1,4 +1,5 @@
 package oop_00000113470_dherylchristoferyahya.week02
+import java.util.Scanner
 
 class Loan(
     val bookTitle: String,
@@ -12,4 +13,31 @@ class Loan(
             0
         }
     }
+}
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+
+    println("--- Library Loan System ---")
+
+    print("Masukkan Judul: ")
+    val title = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) {
+        duration = 1
+    }
+
+    val loanEntry = Loan(title, name, duration)
+
+    println("\n--- Detail Peminjaman ---")
+    println("Judul Buku    : ${loanEntry.bookTitle}")
+    println("Peminjam      : ${loanEntry.borrower}")
+    println("Lama Pinjam   : ${loanEntry.loanDuration} hari")
+    println("Total Denda   : Rp ${loanEntry.calculateFine()}")
 }
