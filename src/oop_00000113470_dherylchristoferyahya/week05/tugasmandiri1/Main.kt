debe28a52e91c834b581037d7356d90da679ef98
DherylC <dheryl.c@gmail.com>
2026-03-03 12:14:01 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000113470_dherylchristoferyahya/week05/tugasmandiri1/Main.kt
+++ src/oop_00000113470_dherylchristoferyahya/week05/tugasmandiri1/Main.kt
@@ -1,2 +1,9 @@
 package oop_00000113470_dherylchristoferyahya.week05.tugasmandiri1
 
+fun main() {
+    val helper = MathHelper()
+
+    println("Luas Persegi: ${helper.hitungLuas(11)}")
+    println("Luas Persegi Panjang: ${helper.hitungLuas(10, 40)}")
+    println("Luas Lingkaran: ${helper.hitungLuas(21.0)}")
+}
