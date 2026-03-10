f15c3f74001888ea810a32b9ec512c76e7d1089f
DherylC <dheryl.c@gmail.com>
2026-03-10 12:26:52 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000113470_dherylchristoferyahya/week06/tugasmandiri01/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package oop_00000113470_dherylchristoferyahya.week06.tugasmandiri01
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} berhasil ditambahkan ke Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        println("Mematikan Seluruh Perangkat Switchable")
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
