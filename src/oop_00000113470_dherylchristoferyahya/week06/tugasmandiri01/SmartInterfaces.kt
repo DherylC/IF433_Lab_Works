737877042987d5954fc62e6ffcfbda9fd2721883
DherylC <dheryl.c@gmail.com>
2026-03-10 12:18:25 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000113470_dherylchristoferyahya/week06/tugasmandiri01/SmartInterfaces.kt
@@ -0,0 +1,19 @@
+package oop_00000113470_dherylchristoferyahya.week06.tugasmandiri01
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
