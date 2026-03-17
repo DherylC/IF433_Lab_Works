8a056babfdf93f55166b4c7ae1b3f82fb2e49378
DherylC <dheryl.c@gmail.com>
2026-03-17 15:18:31 +0700

    week07: implement data class for structural equality

--- src/oop_00000113470_dherylchristoferyahya/week07/Main.kt
+++ src/oop_00000113470_dherylchristoferyahya/week07/Main.kt
@@ -10,8 +10,8 @@ fun main() {
     client.connect()
 
     println("=== TETS REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
 }
