cf5d10b236b6ee80312ba478e2b057d5f6a2c97e
DherylC <dheryl.c@gmail.com>
2026-03-17 15:49:07 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000113470_dherylchristoferyahya/week07/poin/IremRarity.kt
@@ -0,0 +1,9 @@
+package oop_00000113470_dherylchristoferyahya.week07.poin
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
