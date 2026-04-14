08824b481add6904707035e6e3b4630fffd7d86b
DherylC <dheryl.c@gmail.com>
2026-04-14 14:11:05 +0700

    week08: implement java interop using !!

--- src/oop_00000113470_dherylchristoferyahya/week08/Main.kt
+++ src/oop_00000113470_dherylchristoferyahya/week08/Main.kt
@@ -51,6 +51,11 @@ fun main(){
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("status java: $javaResponse, Length: $statusLength")
 }
 
 
