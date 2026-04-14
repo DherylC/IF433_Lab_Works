5c7a943185dc78d9fa3997b8a739966e4d3ccffb
DherylC <dheryl.c@gmail.com>
2026-04-14 14:23:39 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000113470_dherylchristoferyahya/week08/TugasMandiri/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000113470_dherylchristoferyahya.week08.TugasMandiri;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
