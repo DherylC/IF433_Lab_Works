ae25a39f701e3e2d56cfd97f36b4a00cb59a534b
DherylC <dheryl.c@gmail.com>
2026-05-05 11:27:32 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000113470_dherylchristoferyahya/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000113470_dherylchristoferyahya.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
