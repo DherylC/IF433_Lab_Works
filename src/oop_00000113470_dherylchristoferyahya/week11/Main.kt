62b6b6539715307f28d06e444644d49c95c0fa61
DherylC <dheryl.c@gmail.com>
2026-05-05 11:14:20 +0700

    week11: test extension functions in main

---
+++ src/oop_00000113470_dherylchristoferyahya/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000113470_dherylchristoferyahya.week11
+
+fun main(){
+    println("=== TEST EXTENSION FUNCTION")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
