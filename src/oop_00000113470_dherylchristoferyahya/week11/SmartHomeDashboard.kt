2ecca67296c279eae7a356a57409a2be8cc101fa
DherylC <dheryl.c@gmail.com>
2026-05-05 11:31:06 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000113470_dherylchristoferyahya/week11/SmartHomeDashboard.kt
+++ src/oop_00000113470_dherylchristoferyahya/week11/SmartHomeDashboard.kt
@@ -2,4 +2,17 @@ package oop_00000113470_dherylchristoferyahya.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    SmartDevice(name = "", category = "").apply {
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+        println("Sistem: Perangkat '${it.name}' berhasil ditambahkan ke dashboard.")
+    }
+
+    println("\nDaftar Perangkat Saat Ini:")
+    homeDevices.forEach { println(it.diagnose()) }
 }
