784525b2a0dfa86582e3c167b0989826949e3978
DherylC <dheryl.c@gmail.com>
2026-05-05 11:34:19 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000113470_dherylchristoferyahya/week11/SmartHomeDashboard.kt
+++ src/oop_00000113470_dherylchristoferyahya/week11/SmartHomeDashboard.kt
@@ -8,9 +8,7 @@ fun main() {
         category = "Lighting"
         isOnline = true
         powerLoad = 12
-    }.also {
-        homeDevices.add(it)
-    }
+    }.also { homeDevices.add(it) }
 
     SmartDevice("Ezviz Outdoor", "Camera").apply {
         isOnline = true
@@ -19,6 +17,14 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+
+    val acUnit = run {
+        val device = SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+        device
+    }
+    homeDevices.add(acUnit)
+
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
 
     println("\n--- Status Dashboard Smart Home ---")
     homeDevices.forEach { println(it.diagnose()) }
